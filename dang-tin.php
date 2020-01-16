af5d3735470dd2ad08574a56aa6a6a762d978084
= <=>
2020-01-16 17:13:37 +0700

    test đổ dữ liệu ckeditor

--- dang-tin.php
+++ dang-tin.php
@@ -52,7 +52,7 @@ $loailuong = $db->fetchAll('typesalary');
             <!-- Tiêu đề -->
             <div class="form-group">
                 <label>Tên bài viết đăng tuyển <strong class="text-danger">*</strong></label>
-                <input type="text" class="form-control" name="title" placeholder="Nhập tên bài viết đăng tuyển" value="<?php echo isset($title)?$title:''?>">
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nhập tên bài viết đăng tuyển" value="<?php echo isset($title)?$title:''?>">
                 <?php
                 if(isset($errors) && in_array('title', $errors)){
                 echo "<p class='required'>Thông tin về tiêu đề công việc bạn không được để trống</p>";
@@ -280,7 +280,7 @@ $loailuong = $db->fetchAll('typesalary');
             <!-- Thông tin liên hệ -->
             <div class="form-group">
                 <label>Thông tin cho người ứng tuyển liên hệ với bạn <strong class="text-danger">*</strong></label>
-                <textarea class="form-control" name="infoWork" id="" cols="30" rows="3" placeholder="Số điện thoai/ Thông tin liên hệ"
+                <textarea class="form-control" name="infoWork" id="infoWork" cols="30" rows="3" placeholder="Số điện thoai/ Thông tin liên hệ"
                 value="<?php if(isset($_POST['contacts'])){echo $_POST['contacts'];} ?>"
                 ></textarea>
                 <?php
@@ -315,26 +315,35 @@ $loailuong = $db->fetchAll('typesalary');
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Xem mẫu bài đăng</button>
 
                 <!-- Modal -->
-<div class="modal fade" style="padding: 0 !important" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
+                <div class="modal fade" style="padding: 0 !important" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalCenterTitle">Xem mẫu bài đăng</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                                <!-- Chi tiết bài đăng -->
+                                    <div class="form-group">
+                                        <textarea id="noidung2" rows="5" name="description" class="form-control"></textarea>
+                                        <?php
+                                        if(isset($errors) && in_array('description', $errors))
+                                        {
+                                        echo "<p class='required'>Thông Tin Chi Tiết Không Được Để Trống!</p>";
+                                        }
+                                        ?>
+                                    </div>
+                                <!-- End chi tiết bài đăng -->
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
                 <button type="submit" class="btn btn-orange">ĐĂNG TUYỂN VIỆC</button>
             </div>
             </div>
@@ -346,6 +355,16 @@ $loailuong = $db->fetchAll('typesalary');
   </div>
 </section>
 <!-- endsection -->
+<script type="text/javascript" src="admin/ckeditor/ckeditor.js"></script>
+<script type="text/javascript" src="admin/ckfinder/ckfinder.js"></script>
+<script type="text/javascript">
+  CKEDITOR.replace('noidung2');
+</script>
+<script type="text/javascript">
+  CKEDITOR.replace('infoWork');
+</script>
+
+
 <?php
 include('includes/layout_footer.php');
 ?>
